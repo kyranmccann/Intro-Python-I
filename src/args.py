1bd9a6e131256ada1d20dc6b460c7c2b8c40c9fe
kyranmccann <kyran.mccann@gmail.com>
2019-02-11 15:32:53 -0800

    finish args.py

--- src/args.py
+++ src/args.py
@@ -6,12 +6,26 @@
 
 # YOUR CODE HERE
 
+
+def f1(x, y):
+    return x + y
+
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+import functools
+
+
+def f2(*nums):
+    sum = 0
+    for num in nums:
+        sum = sum + num
+    return sum
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -20,14 +34,19 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
-# What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+# What thing do you have to add to make this work? asterisk
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+
+
+def f3(j, k=1):
+    return j + k
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,6 +61,10 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print(f"key: {key}, value: {value}")
+
 
 # Should print
 # key: a, value: 12
@@ -59,5 +82,5 @@ d = {
     "hp": 3
 }
 
-# What thing do you have to add to make this work?
-f4(d)
+# What thing do you have to add to make this work? asterisks
+f4(**d)
